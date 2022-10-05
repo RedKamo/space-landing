ac7a8e3e7eb0cc1a27bde8ce0936bb8099146e0d
Camilo Suarez <camiloandressuarez1@gmail.com>
2022-10-05 13:37:36 -0500

    full responsive technology section

--- src/routes/Technology.jsx
+++ src/routes/Technology.jsx
@@ -8,20 +8,20 @@ const Technology = () => {
   const { name, images, description } = rockets[rocket];
 
   return (
-    <main className=" bg-techno-background bg-cover bg-center h-screen font-bella text-slate-50 pt-44  ">
-      <h1 className="  max-w-7xl mx-auto  pl-24 font-barlow tracking-[0.1em] uppercase text-2xl py-10">
+    <main className=" bg-techno-background bg-cover bg-center min-h-screen font-bella text-slate-50 pt-44  ">
+      <h1 className="  max-w-7xl mx-auto text-center   lg:text-left font-barlow tracking-[0.1em] uppercase text-sm lg:text-2xl py-10">
         {" "}
         <span className=" text-slate-500 font-bold font-barlow">03</span> space
         launche 101
       </h1>
-      <section className="flex  max-w-7xl  mx-auto">
-        <section className="w-full lg:w-2/4 flex items-center gap-4">
-          <article className=" w-20 mx-10">
+      <section className="flex flex-col-reverse md:flex-row md:items-center max-w-7xl lg:h-[530px] lg:justify-around mx-auto gap-8 ">
+        <section className="w-full lg:w-1/2 flex flex-col lg:flex-row  items-center gap-8">
+          <article className=" w-full   flex lg:flex-col lg:w-20 justify-center items-center">
             {rockets.map((rock, index) => (
               <button
                 key={index}
                 onClick={() => setRocket(index)}
-                className={`border-2 border-slate-500 w-20 h-20 my-4 rounded-full text-3xl uppercase font-bella mx-2 tracking-wider text-blue-200 pb-2   ${
+                className={`border-2 flex justify-center items-center border-slate-500 w-10 h-10 lg:w-20 lg:h-20 my-4 rounded-full text-sm lg:text-3xl uppercase font-bella mx-2 tracking-wider text-blue-200    ${
                   index === rocket && " bg-slate-200 text-slate-900 "
                 }`}
               >
@@ -29,20 +29,27 @@ const Technology = () => {
               </button>
             ))}
           </article>
-          <section className="flex flex-col justify-between min-h-max ">
-            <span className=" uppercase font-barlow text-blue-200 pb-4">
+          <section className=" flex flex-col  lg:justify-between items-center h-80 lg:items-start">
+            <span className=" text-sm  uppercase font-barlow text-blue-200 ">
               the terminology ...
             </span>
-            <h1 className="text-[56px] w-[31rem] font-bella uppercase py-2">
+            <h1 className="text-2xl lg:text-[46px]  font-bella text-center uppercase py-2">
               {name}
             </h1>
-            <p className="w-[27.75rem] text-lg font-barlow  text-blue-200 pt-4 leading-8">
+            <p className="  text-center  text-sm lg:text-lg lg:text-left font-barlow  text-blue-200 pt-4 leading-8">
               {description}
             </p>
           </section>
         </section>
         <article className="w-full lg:w-2/4 flex justify-end">
-          <img src={images.portrait} alt="" />
+          <picture>
+            <source media="(min-width: 778px)" srcSet={images.portrait} />
+            <img
+              src={images.landscape}
+              alt=""
+              className="block mx-auto mb-10"
+            />
+          </picture>
         </article>
       </section>
     </main>
